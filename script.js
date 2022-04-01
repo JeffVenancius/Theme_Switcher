300533e99ef4bbbd44083c601eefb12544f90f2b
JeffVenancius <jefferson.venancius@gmail.com>
2022-04-01 11:03:02 -0300

    refactoring.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let on = true
 function toggleSwitch(){
 	let button__themeSwitch = document.getElementsByClassName("button__themeSwitch")[0]
 	let body = document.getElementsByTagName("Body")[0];
-	on = !on;
 	body.classList.toggle("body__night");
 	button__themeSwitch.classList.toggle("button__themeSwitch__off");
 }
